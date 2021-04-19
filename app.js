e8a9adf59c36d4a14b2aefed88fbf10fc52f862b
Tayyab Hussain03 <tayyabfaisal106@gmail.com>
2021-04-19 11:40:30 -0700

    chapter 9-11 complete w/o Q8-Q9

---
+++ app.js
@@ -0,0 +1,199 @@
+// **********CHAPTER 9-11**********
+
+// *****QUESTION 1*****
+
+// var city = prompt("Enter Your City Name")
+// if (city == "karachi") {
+//     document.write("Welcome to the City of Lights")
+// }
+
+// else {
+//     document.write("Welcome to the City")
+// }
+
+// *****QUESTION 2 *****
+
+// var gender = prompt("What is Your Gender")
+// if (gender == "Male") {
+//     document.write("Good Morning! Sir")
+// }
+
+// else if (gender == "Female") {
+//         document.write("Good Morning! Madam")
+// }
+
+// else{
+//     document.write("Good Morning")
+// }
+
+//  *****QUESTION 3*****
+
+// var signalColor = prompt("Enter Signal Color Here")
+// if (signalColor == "Red") {
+//     document.write("Must Stop")
+// }
+
+// else if (signalColor == "Yellow") {
+//     document.write("Ready to Move")
+// }
+
+// else if (signalColor == "Green") {
+//     document.write("Move now")
+// }
+
+// *****QUESTION 4*****
+
+// var fuel = +prompt("Enter Your Remaing Fuel Here(In Litre):")
+// if (fuel<= 0.25 ){
+//     document.write("Please refill the fuel in your car")
+// }
+
+// else{
+//     document.write("No need to Refill the Fuel")
+// }
+
+// *****QUESTION 5a*****
+
+// var a = 4;
+// if (++a === 5) {
+//     alert("given condition for variable a is true");
+// }
+
+// else{
+//     alert("given condition for variable a is false");
+// }
+
+// *****QUESTION 5b*****
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+
+// else {
+//     alert("given condition for variable b is false");
+// }
+
+// *****QUESTION 5c*****
+
+// var c = 12;
+// if (c++ === 13) {
+//     alert("condition 1 is true");
+// }
+// if (c === 13) {
+//     alert("condition 2 is true");
+// }
+// if (++c < 14) {
+//     alert("condition 3 is true");
+// }
+// if (c === 14) {
+//     alert("condition 4 is true");
+// }
+
+// *****QUESTION 5d*****
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals");
+// }
+
+// *****QUESTION 5e*****
+
+// if (true) {
+//     alert("True");
+// }
+// if (false) {
+//     alert("False");
+// }
+
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+
+// *****QUESTION 6*****
+
+// var sub1 = 72
+// var sub2 = 89
+// var sub3 = 95
+// var obtainedMarks = sub1 + sub2 + sub3
+// var percentage = obtainedMarks / 300 * 100
+
+// document.write("<h3>" + "Marks Obtained " + "= " + obtainedMarks + "<h3/>")
+// document.write("<h3>" + "Total Marks " + "=" + 300 + "<h3/>")
+// document.write("<h3>" + "Percentage " + "= " + percentage + "<h3/>")
+
+// if (percentage >= 80) {
+//     document.write("<h3>" + "Grade" + " : " + "A-one" + "<h3/>")
+//     document.write("<h3>" + "Remarks " + ": " + "Excellent")
+// }
+
+// else if (percentage >= 70 && percentage <= 79) {
+//     document.write("<h3>" + "Grade" + ":" + "A" + "<h3/>")
+//     document.write("<h3>" + "Remarks " + ": " + "Good")
+// }
+
+// else if (percentage >= 60 && percentage <= 69) {
+//     document.write("<h3>" + "Grade" + ":" + "B" + "<h3/>")
+//     document.write("<h3>" + "Remarks " + ": " + "You need to improve")
+// }
+
+// else if (percentage >= 2 && percentage <= 59) {
+//     document.write("<h3>" + "Grade" + ":" + "Fail" + "<h3/>")
+//     document.write("<h3>" + "Remarks " + ": " + "Sorry")
+// }
+
+// *****QUESTION 7*****
+
+// var a = +prompt("Guess the number: (1 to 10)")
+// if (a === 6) {
+//     document.write("Bingo! Correct answer")
+// }
+
+// else if (a===5) {
+//     document.write("You are closer to +1")
+// }
+
+// else if (a===7) {
+//     document.write("You are closer to -1")
+// }
+// else{
+//     document.write("Ohhh! You are wrong")
+// }
+
+// *****QUESTION 10*****
+
+// var T = +prompt("Enter Temperature Here")
+// if(T>=40){
+//     document.write("It is too hot outside")
+// }
+// else if (T>=30 && T<=39){
+//     document.write("The Weather Today is Normal")
+// }
+
+// else if (T>=20 && T<=29){
+//     document.write("Today's Weather is Cool")
+// }
+
+// else if (T>=10 && T<=19){
+//     document.write("Today's Weather is so Cool")
+// }
+
+// *****QUESTION 11*****
+
+// var a = +prompt("Enter First Number Here")
+// var b = +prompt("Enter Second Number Here")
+// var answer = a*b
+// var answer = a/b
+// var answer = a-b
+// var answer = a+b
+// var answer = a%b
+// document.write(answer)
+
+
+
+
+
+
+
